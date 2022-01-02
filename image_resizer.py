7547e842ec7ac2f3509dd2031363ed3fcd165912
FAT-PANDA90 <37924349+FAT-PANDA90@users.noreply.github.com>
2022-01-03 04:01:02 +0530

    Image resizer

---
+++ image_resizer.py
@@ -0,0 +1,46 @@
+__author__ = "SABYASACHI SHARMA"
+__title__ = "ScanImage Resizer"
+import os
+from PIL import Image
+import zipfile
+
+
+class BColors:
+    HEADER = '\033[95m'
+    OK_BLUE = '\033[94m'
+    OK_CYAN = '\033[96m'
+    OK_GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    END_C = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+try:
+    print(f"Programmed by Sabyasachi Sharma (835148)")
+    print(f"Keep all the files in one folder and run this program")
+    print(f"Warning: all images will be replaced with resized images bearing "
+          f"filename:-edited_original_file_name")
+    input("Press enter when ready")
+except SyntaxError:
+    pass
+to_delete = []
+for file in os.listdir():
+    if file.endswith('.jpg'):
+        im = Image.open(file)
+        new_im = im.resize((620, 870), Image.ANTIALIAS)
+        new_im.save(f'edited_{os.path.splitext(file.title())[0]}.jpg')
+        to_delete.append(file.title())
+
+zf = zipfile.ZipFile('originals.zip', mode='w') # zipping original files and then deleting them
+for file in os.listdir():
+    if file.title() in to_delete:
+        zf.write(file)
+        os.remove(file)
+zf.close()
+try:
+    print("original files replaced with new resized images")
+    input("Press enter to exit")
+except SyntaxError:
+    pass
